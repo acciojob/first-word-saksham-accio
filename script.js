110f2f688ba388a682c5e662eacb0ab81a22979a
saksham-accio <125554544+saksham-accio@users.noreply.github.com>
2024-02-28 11:05:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function firstWord(s) {
   // your code here
+	let res = s.trim().split(" ");
+	return res[0];
+	
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
+const s = prompt("Enter String:")
 alert(firstWord(s));
