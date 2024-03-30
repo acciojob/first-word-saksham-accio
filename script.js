d41a2201eb532fcd80c180127f89e89c5f001a76
saksham-accio <125554544+saksham-accio@users.noreply.github.com>
2024-03-30 12:43:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 function firstWord(s) {
-  // your code here
-	let res = s.trim().split(" ");
-	return res[0];
-	
+  s = s.trim() + " ";
+  let firstBlank = s.indexOf(" ");
+  return s.substr(0, firstBlank);
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:")
+const s = prompt("Enter String:");
 alert(firstWord(s));
